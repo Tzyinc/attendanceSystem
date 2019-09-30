804c226178063be1cfe639e61041a423382a9e90
Zhi Yang Ten <tenzy@seagroup.com>
2019-09-30 22:21:43 +0800

    with registration

--- qrScanner/index.js
+++ qrScanner/index.js
@@ -1,9 +1,12 @@
 
+let groupId;
+let member;
 
 async function app() {
     let scanner = new Instascan.Scanner({ video: document.getElementById('webcam') });
     scanner.addListener('scan', function (content) {
-        console.log(content);
+        document.getElementById('loading').style.display = 'block';
+        fetchData(content)
     });
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
@@ -14,6 +17,69 @@ async function app() {
     }).catch(function (e) {
         console.error(e);
     });
+}
+const FIELDS = ['cat', 'fname', 'lname', 'group', 'teamName', 'numMembers', 'shirtSize'];
+function fetchData(ids) {
+    let idInfo = ids.split('-');
+    groupId = idInfo[0];
+    member = idInfo[1];
+    fetch(`http://165.22.245.206:8081/getUserData?id=${groupId}&member=${member}`)
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    alert('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+
+                // Examine the text in the response
+                response.json().then(function (data) {
+                    console.log(data);
+                    document.getElementById('loading').style.display = 'none';
+                    document.getElementById('applicantInfo').style.display = "block";
+                    
+                    for (let field of FIELDS) {
+                        document.getElementById(field).innerText = data[field];
+                    }
+                });
+            }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+            alert('Looks like there was a problem' + err);
+        });
+}
+
+function confirmRegistration() {
+    console.log(groupId,member);
+    fetch(`http://165.22.245.206:8081/submitUserAttendance?id=${groupId}&member=${member}`)
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    alert('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+
+                // Examine the text in the response
+                response.json().then(function (data) {
+                    console.log(data);
+                    if (data && data.success) {
+
+                        document.getElementById('applicantInfo').style.display = "none";
+                        groupId = undefined;
+                        member = undefined;
+                        alert('success!');
+                    } else {
+                        alert('error: ' + data)
+                    }
+                });
+            }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+            alert('Looks like there was a problem' + err);
+        });
 }
 
 app();
